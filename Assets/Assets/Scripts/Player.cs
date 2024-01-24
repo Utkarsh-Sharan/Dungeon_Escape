e3cfe06de1b7dabc7fc7a52396f1f80279cc439d
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-01-24 12:38:47 +0530

    set player movement/jump

---
+++ Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+
+    private float _jumpForce = 5.0f;
+    private bool _resetJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        _rigid.velocity = new Vector2(move, _rigid.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
+        {
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            StartCoroutine(ResetJumpRoutine());
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, 1<<8);
+        if(hitInfo.collider != null)
+        {
+            if(_resetJump == false) 
+                return true;
+        }
+
+        return false;
+    }
+
+    IEnumerator ResetJumpRoutine()
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        _resetJump = false;
+    }
+}
