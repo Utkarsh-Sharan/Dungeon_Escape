df2e3c138bac85353052d06c589941c514841a15
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-01-25 12:09:16 +0530

    jump/run animation

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -6,13 +6,18 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigid;
 
+    private float _moveSpeed = 3.5f;
     private float _jumpForce = 5.0f;
     private bool _resetJump = false;
+    private bool _grounded = false;
+
+    private PlayerAnimation _playerAnimation;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
+        _playerAnimation = GetComponent<PlayerAnimation>();
     }
 
     // Update is called once per frame
@@ -24,22 +29,30 @@ public class Player : MonoBehaviour
     void Movement()
     {
         float move = Input.GetAxisRaw("Horizontal");
-        _rigid.velocity = new Vector2(move, _rigid.velocity.y);
+        _grounded = IsGrounded();
 
         if(Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
         {
             _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
             StartCoroutine(ResetJumpRoutine());
+            _playerAnimation.Jump(true);
         }
+
+        _rigid.velocity = new Vector2(move * _moveSpeed, _rigid.velocity.y);
+        _playerAnimation.Move(move);
     }
 
     private bool IsGrounded()
     {
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, 1<<8);
+        Debug.DrawRay(transform.position, Vector2.down, Color.red);
         if(hitInfo.collider != null)
         {
-            if(_resetJump == false) 
+            if(_resetJump == false)
+            {
+                _playerAnimation.Jump(false);
                 return true;
+            }
         }
 
         return false;
