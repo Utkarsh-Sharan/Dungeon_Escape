4cc320f24a8a34d366121eb9b0ef8fab3847be69
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-02-08 11:32:44 +0530

    damage interface added

--- Assets/Assets/Scripts/Enemy/Skeleton.cs
+++ Assets/Assets/Scripts/Enemy/Skeleton.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skeleton : Enemy
+public class Skeleton : Enemy, IDamageable
 {
+    public int Health { get; set; }
+
     //use this for initialization
     public override void Init()
     {
         base.Init();
+    }
+
+    public void Damage()
+    {
+
     }
 }
